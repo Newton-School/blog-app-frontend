7df0267ca3d448f307878b57b386afd7aad4ede6
romil jain <romiljain258@gmail.com>
2020-09-26 16:34:02 +0530

    changes

--- src/components/EachBlog.jsx
+++ src/components/EachBlog.jsx
@@ -1,6 +1,16 @@
 import React from 'react';
 
-
+const EachBlog = (props) => {
+    return (
+        <>
+            {props.arr.map((val, i) => (<div key={i} className="block">
+                <span className="text">{val}</span>
+                <button onClick={() => { props.editBlog(i) }}>Edit</button>
+                <button onClick={() => { props.deletBlog(i) }}>Delete</button>
+            </div>
+            ))}
+        </>)
+}
 
 
 
